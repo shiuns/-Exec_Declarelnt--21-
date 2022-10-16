4fa7b99368ed191006b1e4e9cfc17e899619145a
Shiun <Shiun@DESKTOP-KPBEE4S>
2022-10-16 18:05:16 +0800

    加入專案檔案。

---
+++ [作業(Exec_Declarelnt 第一題計算)]-21謝孟勳/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _作業_Exec_Declarelnt_第一題計算___21謝孟勳
+{
+    //計算1+(2+2)*3-1
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1 + (2 + 2) * 3 - 1;
+            Console.WriteLine(number);
+        }
+    }
+}
